42516468f9a5039e1ac64a0b223d0f49450ddee1
Gustavo Gonzaga <104742584+gonzagaa@users.noreply.github.com>
2024-01-18 17:00:48 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+    showButtonHomeOnScroll()
+}
+
+function showButtonHomeOnScroll() {
+    if(scrollY > 200) {
+        document.querySelector("#buttonHome").classList.add("show")
+    } else {
+        document.querySelector("#buttonHome").classList.remove("show")
+    }
+}
+
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "flip",
+    grabCursor: true,
+  });
