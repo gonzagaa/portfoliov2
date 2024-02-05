7b28860432d0db0e2ab5615a2a5a3d5288550ea0
gonzagaa <iigust4vo@gmail.com>
2024-02-05 16:51:01 -0300

    update

--- script.js
+++ script.js
@@ -21,17 +21,31 @@ function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
 
+ScrollReveal().reveal('#home h1, #home p', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out', delay: 200});
+ScrollReveal().reveal('.social-links .in', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out', delay: 0});
+ScrollReveal().reveal('.social-links .git', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out', delay: 100});
+ScrollReveal().reveal('.social-links .ig', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out', delay: 200});
+
+ScrollReveal().reveal('#projects .left, header h2, header p, #sobre .image', { distance: '400px', origin: 'left', duration: 500, easing: 'ease-in-out'});
+ScrollReveal().reveal('#projects .right, #sobre .text', { distance: '400px', origin: 'right', duration: 500, easing: 'ease-in-out'});
+
+ScrollReveal().reveal('#competenciasDesktop .one', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out', mobile: false, delay: 50});
+ScrollReveal().reveal('#competenciasDesktop .two', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out', mobile: false, delay: 150});
+ScrollReveal().reveal('#competenciasDesktop .three', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out', mobile: false, delay: 250});
+ScrollReveal().reveal('#competenciasDesktop .for', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out', mobile: false, delay: 350});
+
+
 var swiper = new Swiper(".mySwiper", {
-    effect: "flip",
     grabCursor: true,
-    pagination: {
-        el: '.swiper-pagination',
+      effect: "creative",
+      creativeEffect: {
+        prev: {
+          shadow: true,
+          translate: ["-120%", 0, -500],
+        },
+        next: {
+          shadow: true,
+          translate: ["120%", 0, -500],
+        },
       },
-    
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    
   });
